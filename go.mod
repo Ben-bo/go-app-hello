3f49e3a1c4e131c4bcff7452e173a04645911d27
Beni Iskandar <bennyizkandar@gmail.com>
2023-09-27 11:31:53 +0800

    int module say hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Ben-bo/go-app-hello
+
+go 1.21.1
+
+require github.com/Ben-bo/go-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	say_hello "github.com/Ben-bo/go-hello"
+)
+
+func main() {
+	fmt.Println(say_hello.SayHello("beni"))
+}
